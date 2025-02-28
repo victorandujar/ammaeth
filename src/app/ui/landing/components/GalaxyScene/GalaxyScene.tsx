e31154419dc3bb5f777cbaab6e0a4a6260548de4
victorandujar <victorandujarmurcia@gmail.com>
2025-02-28 09:08:01 +0100

    Modify landing compos of Galaxy to implement scroll transition

--- src/app/ui/landing/components/GalaxyScene/GalaxyScene.tsx
+++ src/app/ui/landing/components/GalaxyScene/GalaxyScene.tsx
@@ -1,15 +1,19 @@
-"use client";
-
-import { Canvas } from "@react-three/fiber";
-import Galaxy from "../Galaxy/Galaxy";
+import { Camera, Canvas } from "@react-three/fiber";
+import { useState } from "react";
+import GalaxyAnimation from "../GalaxyAnimation/GalaxyAnimation";
 
 const GalaxyScene: React.FC = () => {
+  const [camera, setCamera] = useState<Camera | null>(null);
+
   return (
     <div className="w-full h-full bg-transparent">
-      <Canvas camera={{ position: [0, 0, 150], fov: 60 }}>
+      <Canvas
+        camera={{ position: [0, 0, 150], fov: 60 }}
+        onCreated={({ camera }) => setCamera(camera)}
+      >
         <ambientLight intensity={0.6} />
         <pointLight position={[0, 0, 0]} intensity={2} color="#fffec0" />
-        <Galaxy />
+        {camera && <GalaxyAnimation camera={camera} />}
       </Canvas>
     </div>
   );
